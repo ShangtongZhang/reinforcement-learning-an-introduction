1f190d5cfe09098e1eeed3b8a8454cf4d34d7c85
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-09-19 13:29:19 -0600

    Finish chapter 3, grid world

---
+++ chapter3/GridWorld.py
@@ -0,0 +1,81 @@
+from utils import *
+import numpy as np
+
+WORLD_SIZE = 5
+A_POS = [0, 1]
+A_PRIME_POS = [4, 1]
+B_POS = [0, 3]
+B_PRIME_POS = [2, 3]
+discount = 0.9
+
+world = np.zeros((WORLD_SIZE, WORLD_SIZE))
+
+# left, up, right, down
+actions = ['L', 'U', 'R', 'D']
+
+actionProb = []
+for i in range(0, WORLD_SIZE):
+    actionProb.append([])
+    for j in range(0, WORLD_SIZE):
+        actionProb[i].append(dict({'L':0.25, 'U':0.25, 'R':0.25, 'D':0.25}))
+
+nextState = []
+actionReward = []
+for i in range(0, WORLD_SIZE):
+    nextState.append([])
+    actionReward.append([])
+    for j in range(0, WORLD_SIZE):
+        next = dict()
+        reward = dict()
+        if i == 0:
+            next['U'] = [i, j]
+            reward['U'] = -1.0
+        else:
+            next['U'] = [i - 1, j]
+            reward['U'] = 0.0
+
+        if i == WORLD_SIZE - 1:
+            next['D'] = [i, j]
+            reward['D'] = -1.0
+        else:
+            next['D'] = [i + 1, j]
+            reward['D'] = 0.0
+
+        if j == 0:
+            next['L'] = [i, j]
+            reward['L'] = -1.0
+        else:
+            next['L'] = [i, j - 1]
+            reward['L'] = 0.0
+
+        if j == WORLD_SIZE - 1:
+            next['R'] = [i, j]
+            reward['R'] = -1.0
+        else:
+            next['R'] = [i, j + 1]
+            reward['R'] = 0.0
+
+        if [i, j] == A_POS:
+            next['L'] = next['R'] = next['D'] = next['U'] = A_PRIME_POS
+            reward['L'] = reward['R'] = reward['D'] = reward['U'] = 10.0
+
+        if [i, j] == B_POS:
+            next['L'] = next['R'] = next['D'] = next['U'] = B_PRIME_POS
+            reward['L'] = reward['R'] = reward['D'] = reward['U'] = 5.0
+
+        nextState[i].append(next)
+        actionReward[i].append(reward)
+
+while True:
+    # keep iteration until convergence
+    newWorld = np.zeros((WORLD_SIZE, WORLD_SIZE))
+    for i in range(0, WORLD_SIZE):
+        for j in range(0, WORLD_SIZE):
+            for action in actions:
+                newPosition = nextState[i][j][action]
+                # bellman equation
+                newWorld[i, j] += actionProb[i][j][action] * (actionReward[i][j][action] + discount * world[newPosition[0], newPosition[1]])
+    if np.abs(np.sum(world - newWorld)) < 1e-4:
+        print newWorld
+        break
+    world = newWorld
