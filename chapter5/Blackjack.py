d008275876dda7fb1bdf6faa3093cfc2fba67611
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-10-05 13:44:14 -0600

    Mean squared error between ordinary importance sampling and weighted importance sampling

--- chapter5/Blackjack.py
+++ chapter5/Blackjack.py
@@ -21,6 +21,16 @@ for i in range(12, 20):
 policyPlayer[20] = ACTION_STAND
 policyPlayer[21] = ACTION_STAND
 
+# function form of target policy of player
+def targetPolicyPlayer(playerSum):
+    return policyPlayer[playerSum]
+
+# function form of behavior policy of player
+def behaviorPolicyPlayer(playerSum):
+    if np.random.binomial(1, 0.5) == 1:
+        return ACTION_STAND
+    return ACTION_HIT
+
 # policy for dealer
 policyDealer = np.zeros(22)
 for i in range(12, 17):
@@ -35,49 +45,89 @@ def getCard():
     return card
 
 # play a game
-def play():
+# @policyPlayerFn: specify policy for player
+# @initialState: [whether player has a usable Ace, sum of player's cards, one card of dealer]
+def play(policyPlayerFn, initialState=None):
+    # player status
+
     # sum of player
     playerSum = 0
 
-    numOfAce = 0
+    # trajectory of player
+    playerTrajectory = []
 
     # whether player uses Ace as 11
-    usableAce = False
+    usableAcePlayer = False
 
-    # initialize cards of player
-    while playerSum < 12:
-        # if sum of player is less than 12, always hit
-        card = getCard()
+    # dealer status
+    dealerCard1 = 0
+    dealerCard2 = 0
+    usableAceDealer = False
 
-        # if get an Ace, use it as 11
-        if card == 1:
-            numOfAce += 1
-            card = 11
-            usableAce = True
-        playerSum += card
+    if initialState is None:
+        # generate a random initial state
 
-    # if player's sum is larger than 21, he must hold at least one Ace, two Aces are possible
-    if playerSum > 21:
-        # use the Ace as 1 rather than 11
-        playerSum -= 10
+        numOfAce = 0
 
-        # if the player only has one Ace, then he doesn't have usable Ace any more
-        if numOfAce == 1:
-            usableAce = False
+        # initialize cards of player
+        while playerSum < 12:
+            # if sum of player is less than 12, always hit
+            card = getCard()
 
-    # initialize cards of dealer, suppose dealer will show the first card ha gets
-    dealerCard1 = getCard()
-    dealerCard2 = getCard()
+            # if get an Ace, use it as 11
+            if card == 1:
+                numOfAce += 1
+                card = 11
+                usableAcePlayer = True
+            playerSum += card
 
-    # starting state of player
-    state = [usableAce, playerSum, dealerCard1]
+        # if player's sum is larger than 21, he must hold at least one Ace, two Aces are possible
+        if playerSum > 21:
+            # use the Ace as 1 rather than 11
+            playerSum -= 10
+
+            # if the player only has one Ace, then he doesn't have usable Ace any more
+            if numOfAce == 1:
+                usableAcePlayer = False
+
+        # initialize cards of dealer, suppose dealer will show the first card he gets
+        dealerCard1 = getCard()
+        dealerCard2 = getCard()
+
+    else:
+        # use specified initial state
+        usableAcePlayer = initialState[0]
+        playerSum = initialState[1]
+        dealerCard1 = initialState[2]
+        dealerCard2 = getCard()
+
+    # initial state of the game
+    state = [usableAcePlayer, playerSum, dealerCard1]
+
+    # initialize dealer's sum
+    dealerSum = 0
+    if dealerCard1 == 1 and dealerCard2 != 1:
+        dealerSum += 11 + dealerCard2
+        usableAceDealer = True
+    elif dealerCard1 != 1 and dealerCard2 == 1:
+        dealerSum += dealerCard1 + 11
+        usableAceDealer = True
+    elif dealerCard1 == 1 and dealerCard2 == 1:
+        dealerSum += 1 + 11
+        usableAceDealer = True
+    else:
+        dealerSum += dealerCard1 + dealerCard2
 
     # game starts!
 
     # player't turn
     while True:
         # get action based on current sum
-        action = policyPlayer[playerSum]
+        action = policyPlayerFn(playerSum)
+
+        # track player's trajectory for importance sampling
+        playerTrajectory.append([action, playerSum])
+
         if action == ACTION_STAND:
             break
         # if hit, get new card
@@ -86,27 +136,12 @@ def play():
         # player busts
         if playerSum > 21:
             # if player has a usable Ace, use it as 1 to avoid busting and continue
-            if usableAce == True:
+            if usableAcePlayer == True:
                 playerSum -= 10
-                usableAce = False
+                usableAcePlayer = False
             else:
                 # otherwise player loses
-                return state, -1
-
-    # initialize dealer's sum
-    dealerSum = 0
-    usableAce = False
-    if dealerCard1 == 1 and dealerCard2 != 1:
-        dealerSum += 11 + dealerCard2
-        usableAce = True
-    elif dealerCard1 != 1 and dealerCard2 == 1:
-        dealerSum += dealerCard1 + 11
-        usableAce = True
-    elif dealerCard1 == 1 and dealerCard2 == 1:
-        dealerSum += 1 + 11
-        usableAce = True
-    else:
-        dealerSum += dealerCard1 + dealerCard2
+                return state, -1, playerTrajectory
 
     # dealer's turn
     while True:
@@ -118,24 +153,24 @@ def play():
         dealerSum += getCard()
         # dealer busts
         if dealerSum > 21:
-            if usableAce == True:
+            if usableAceDealer == True:
             # if dealer has a usable Ace, use it as 1 to avoid busting and continue
                 dealerSum -= 10
-                usableAce = False
+                usableAceDealer = False
             else:
             # otherwise dealer loses
-                return state, 1
+                return state, 1, playerTrajectory
 
     # compare the sum between player and dealer
     if playerSum > dealerSum:
-        return state, 1
+        return state, 1, playerTrajectory
     elif playerSum == dealerSum:
-        return state, 0
+        return state, 0, playerTrajectory
     else:
-        return state, -1
+        return state, -1, playerTrajectory
 
-# Monte Carlo Sample
-def monteCarlo(nEpisodes):
+# Monte Carlo Sample with On-Policy
+def monteCarloOnPolicy(nEpisodes):
     statesUsableAce = np.zeros((10, 10))
     # initialze counts to 1 to avoid 0 being divided
     statesUsableAceCount = np.ones((10, 10))
@@ -143,7 +178,7 @@ def monteCarlo(nEpisodes):
     # initialze counts to 1 to avoid 0 being divided
     statesNoUsableAceCount = np.ones((10, 10))
     for i in range(0, nEpisodes):
-        state, reward = play()
+        state, reward, _ = play(targetPolicyPlayer)
         state[1] -= 12
         state[2] -= 1
         if state[0]:
@@ -154,8 +189,36 @@ def monteCarlo(nEpisodes):
             statesNoUsableAce[state[1], state[2]] += reward
     return statesUsableAce / statesUsableAceCount, statesNoUsableAce / statesNoUsableAceCount
 
-statesUsableAce1, statesNoUsableAce1 = monteCarlo(10000)
-statesUsableAce2, statesNoUsableAce2 = monteCarlo(500000)
+# Monte Carlo Sample with Off-Policy
+def monteCarloOffPolicy(nEpisodes):
+    initialState = [True, 13, 2]
+    sumOfImportanceRatio = [0]
+    sumOfRewards = [0]
+    for i in range(0, nEpisodes):
+        _, reward, playerTrajectory = play(behaviorPolicyPlayer, initialState=initialState)
+
+        # get the importance ratio
+        importanceRatioAbove = 1.0
+        importanceRatioBelow = 1.0
+        for action, playerSum in playerTrajectory:
+            if action == targetPolicyPlayer(playerSum):
+                importanceRatioBelow *= 0.5
+            else:
+                importanceRatioAbove = 0.0
+                break
+        importanceRatio = importanceRatioAbove / importanceRatioBelow
+        sumOfImportanceRatio.append(sumOfImportanceRatio[-1] + importanceRatio)
+        sumOfRewards.append(sumOfRewards[-1] + reward * importanceRatio)
+    del sumOfImportanceRatio[0]
+    del sumOfRewards[0]
+    ordinarySampling = np.asarray(sumOfRewards) / np.arange(1, nEpisodes + 1)
+    weightedSampling = []
+    for sumOfReward_, sumOfRatio_ in zip(sumOfRewards, sumOfImportanceRatio):
+        if sumOfRatio_ == 0:
+            weightedSampling.append(0)
+        else:
+            weightedSampling.append(sumOfReward_ / sumOfRatio_)
+    return ordinarySampling, np.asarray(weightedSampling)
 
 # print the state value
 figureIndex = 0
@@ -178,10 +241,38 @@ def prettyPrint(data, tile):
     ax.set_ylabel('dealer showing')
     ax.set_zlabel('reward')
 
-prettyPrint(statesUsableAce1, 'Usable Ace, 10000 Episodes')
-prettyPrint(statesNoUsableAce1, 'No Usable Ace, 10000 Episodes')
-prettyPrint(statesUsableAce2, 'Usable Ace, 500000 Episodes')
-prettyPrint(statesNoUsableAce2, 'No Usable Ace, 500000 Episodes')
-plt.show()
+# Figure 5.1
+def onPolicy():
+    statesUsableAce1, statesNoUsableAce1 = monteCarloOnPolicy(10000)
+    statesUsableAce2, statesNoUsableAce2 = monteCarloOnPolicy(500000)
+    prettyPrint(statesUsableAce1, 'Usable Ace, 10000 Episodes')
+    prettyPrint(statesNoUsableAce1, 'No Usable Ace, 10000 Episodes')
+    prettyPrint(statesUsableAce2, 'Usable Ace, 500000 Episodes')
+    prettyPrint(statesNoUsableAce2, 'No Usable Ace, 500000 Episodes')
+    plt.show()
 
+# Figure 5.4
+def offPolicy():
+    trueValue = -0.27726
+    nEpisodes = 10000
+    nRuns = 100
+    ordinarySampling = np.zeros(nEpisodes)
+    weightedSampling = np.zeros(nEpisodes)
+    for i in range(0, nRuns):
+        ordinarySampling_, weightedSampling_ = monteCarloOffPolicy(nEpisodes)
+        # get the squared error
+        ordinarySampling += np.power(ordinarySampling_ - trueValue, 2)
+        weightedSampling += np.power(weightedSampling_ - trueValue, 2)
+    ordinarySampling /= nRuns
+    weightedSampling /= nRuns
+    axisX = np.log10(np.arange(1, nEpisodes + 1))
+    plt.plot(axisX, ordinarySampling, label='Ordinary Importance Sampling')
+    plt.plot(axisX, weightedSampling, label='Weighted Importance Sampling')
+    plt.xlabel('Episodes (10^x)')
+    plt.ylabel('Mean square error')
+    plt.legend()
+    plt.show()
+
+# onPolicy()
+offPolicy()
 
