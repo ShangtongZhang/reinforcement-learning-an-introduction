3614d1ad951d0c782d398b46cc76838e43422055
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-10-05 15:05:05 -0600

    Variance for ordinary importance sampling

---
+++ chapter5/InfiniteVariance.py
@@ -0,0 +1,55 @@
+#######################################################################
+# Copyright (C) 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import numpy as np
+from utils import *
+import matplotlib.pyplot as plt
+
+ACTION_BACK = 0
+ACTION_END = 1
+
+# behavior policy
+def behaviorPolicy():
+    return np.random.binomial(1, 0.5)
+
+# target policy
+def targetPolicy():
+    return ACTION_BACK
+
+# one turn
+def play():
+    # track the action for importance ratio
+    trajectory = []
+    while True:
+        action = behaviorPolicy()
+        trajectory.append(action)
+        if action == ACTION_END:
+            return 0, trajectory
+        if np.random.binomial(1, 0.9) == 0:
+            return 1, trajectory
+
+def monteCarloSample():
+    runs = 10
+    episodes = 100000
+    axisX = np.log10(np.arange(1, episodes + 1))
+    for run in range(0, runs):
+        sumOfRewards = [0]
+        for episode in range(0, episodes):
+            reward, trajectory = play()
+            if trajectory[-1] == ACTION_END:
+                importanceRatio = 0
+            else:
+                importanceRatio = 1.0 / pow(0.5, len(trajectory))
+            sumOfRewards.append(sumOfRewards[-1] + importanceRatio * reward)
+        del sumOfRewards[0]
+        estimations = np.asarray(sumOfRewards) / np.arange(1, episodes + 1)
+        plt.plot(axisX, estimations)
+    plt.xlabel('Episodes (10^x)')
+    plt.ylabel('Ordinary Importance Sampling')
+    plt.show()
+    return
+
+monteCarloSample()
