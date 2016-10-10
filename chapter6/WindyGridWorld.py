2c05fa866f0f9a8030452aefa29b6412037a05b3
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-10-10 11:36:19 -0600

    Windy Grid World for chapter 6

---
+++ chapter6/WindyGridWorld.py
@@ -0,0 +1,119 @@
+#######################################################################
+# Copyright (C) 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import numpy as np
+from utils import *
+import matplotlib.pyplot as plt
+
+# world height
+WORLD_HIGHT = 7
+
+# world width
+WORLD_WIDTH = 10
+
+# wind strength for each column
+WIND = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
+
+# possible actions
+ACTION_UP = 0
+ACTION_DOWN = 1
+ACTION_LEFT = 2
+ACTION_RIGHT = 3
+
+# probability for exploration
+EPSILON = 0.1
+
+# Sarsa step size
+ALPHA = 0.5
+
+# reward for each step
+REWARD = -1.0
+
+# state action pair value
+stateActionValues = np.zeros((WORLD_HIGHT, WORLD_WIDTH, 4))
+startState = [3, 0]
+goalState = [3, 7]
+actions = [ACTION_UP, ACTION_DOWN, ACTION_LEFT, ACTION_RIGHT]
+
+# set up destinations for each action in each state
+actionDestination = []
+for i in range(0, WORLD_HIGHT):
+    actionDestination.append([])
+    for j in range(0, WORLD_WIDTH):
+        destination = dict()
+        destination[ACTION_UP] = [max(i - 1 - WIND[j], 0), j]
+        destination[ACTION_DOWN] = [max(min(i + 1 - WIND[j], WORLD_HIGHT - 1), 0), j]
+        destination[ACTION_LEFT] = [max(i - WIND[j], 0), max(j - 1, 0)]
+        destination[ACTION_RIGHT] = [max(i - WIND[j], 0), min(j + 1, WORLD_WIDTH - 1)]
+        actionDestination[-1].append(destination)
+
+# play for an episode
+def oneEpisode():
+    # track the total time steps in this episode
+    time = 0
+
+    # initialize state
+    currentState = startState
+
+    # choose an action based on epsilon-greedy algorithm
+    if np.random.binomial(1, EPSILON) == 1:
+        currentAction = np.random.choice(actions)
+    else:
+        currentAction = argmax(stateActionValues[currentState[0], currentState[1], :])
+
+    # keep going until get to the goal state
+    while currentState != goalState:
+        newState = actionDestination[currentState[0]][currentState[1]][currentAction]
+        if np.random.binomial(1, EPSILON) == 1:
+            newAction = np.random.choice(actions)
+        else:
+            newAction = argmax(stateActionValues[newState[0], newState[1], :])
+        # Sarsa update
+        stateActionValues[currentState[0], currentState[1], currentAction] += \
+            ALPHA * (REWARD + stateActionValues[newState[0], newState[1], newAction] -
+            stateActionValues[currentState[0], currentState[1], currentAction])
+        currentState = newState
+        currentAction = newAction
+        time += 1
+    return time
+
+# play for 500 episodes to make sure to get a more converged policy
+# episodeLimit = 200
+
+# figure 6.4
+episodeLimit = 500
+ep = 0
+episodes = []
+while ep < episodeLimit:
+    time = oneEpisode()
+    episodes.extend([ep] * time)
+    ep += 1
+
+plt.figure()
+plt.plot(episodes)
+plt.show()
+
+# display the optimal policy
+optimalPolicy = []
+for i in range(0, WORLD_HIGHT):
+    optimalPolicy.append([])
+    for j in range(0, WORLD_WIDTH):
+        if [i, j] == goalState:
+            optimalPolicy[-1].append('G')
+            continue
+        bestAction = argmax(stateActionValues[i, j, :])
+        if bestAction == ACTION_UP:
+            optimalPolicy[-1].append('U')
+        elif bestAction == ACTION_DOWN:
+            optimalPolicy[-1].append('D')
+        elif bestAction == ACTION_LEFT:
+            optimalPolicy[-1].append('L')
+        elif bestAction == ACTION_RIGHT:
+            optimalPolicy[-1].append('R')
+for row in optimalPolicy:
+    print row
+print [str(w) for w in WIND]
+
