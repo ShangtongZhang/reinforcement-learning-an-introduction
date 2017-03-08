bd5f8359463f2e36cb203532aa96a17fdb135122
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-03-07 21:31:10 -0700

    add contributor

--- chapter04/CarRental.py
+++ chapter04/CarRental.py
@@ -1,8 +1,8 @@
 #######################################################################
 # Copyright (C)                                                       #
 # 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
-# 2016 Kenta Shimada(hyperkentakun@gmail.com)
-# 2017 Aja Rangaswamy (aja004@gmail.com)                         #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
+# 2017 Aja Rangaswamy (aja004@gmail.com)                              #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
