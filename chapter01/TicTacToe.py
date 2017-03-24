d44c85d374025738b04c27260a48eef5d312c97e
kentan <hyperkentakun@gmail.com>
2017-03-24 10:04:41 -0600

    Updated contrinution list.

--- chapter01/TicTacToe.py
+++ chapter01/TicTacToe.py
@@ -3,6 +3,7 @@
 # 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
 # 2016 Jan Hakenberg(jan.hakenberg@gmail.com)                         #
 # 2016 Tian Jun(tianjun.cpp@gmail.com)                                #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
