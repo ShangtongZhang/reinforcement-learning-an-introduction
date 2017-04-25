32a282382762006cebaa928e6da48587fdde28a2
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-04-25 13:35:17 -0600

    Figure 11.2 Baird's counterexample

--- chapter11/Baird.py
+++ chapter11/Baird.py
@@ -12,14 +12,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # all states: state 0-5 are upper states
-STATES = np.arange(0, 8)
+STATES = np.arange(0, 7)
 # state 6 is lower state
 LOWER_STATE = 6
-# state 7 is terminal state
-TERMINAL_STATE = 7
-
-# probability to the terminal state for a transition
-TERMINAL_PROBABILITY = 0.01
+# discount factor
+DISCOUNT = 0.99
 
 # each state is represented by a vector of length 8
 FEATURE_SIZE = 8
@@ -35,10 +32,11 @@ DASHED = 0
 SOLID = 1
 ACTIONS = [DASHED, SOLID]
 
+# reward is always zero
+REWARD = 0
+
 # take @action at @state, return the new state
 def takeAction(state, action):
-    if np.random.binomial(1, TERMINAL_PROBABILITY) == 1:
-        return TERMINAL_STATE
     if action == SOLID:
         return LOWER_STATE
     return np.random.choice(STATES[: LOWER_STATE])
@@ -55,47 +53,92 @@ def behaviorPolicy(state):
     return DASHED
 
 # Semi-gradient off-policy temporal difference
+# @state: current state
 # @weights: weight for each component of the feature vector
 # @alpha: step size
-def semiGradientOffPolicyTD(weights, alpha):
-    # start from a random state
-    currentState = np.random.choice(STATES[: TERMINAL_STATE])
-    while currentState != TERMINAL_STATE:
-        action = behaviorPolicy(currentState)
-        newState = takeAction(currentState, action)
-        # get the importance ration
-        if action == DASHED:
-            rho = 0.0
-        else:
-            rho = 1.0 / BEHAVIOR_SOLID_PROBABILITY
-        delta = np.dot(FEATURES[newState, :], weights) - \
-                np.dot(FEATURES[currentState, :], weights)
-        delta *= rho * alpha
-        # derivatives happen to be the same matrix due to the linearity
-        weights += FEATURES[currentState, :] * delta
-        currentState = newState
+# @return: next state
+def semiGradientOffPolicyTD(state, weights, alpha):
+    action = behaviorPolicy(state)
+    nextState = takeAction(state, action)
+    # get the importance ration
+    if action == DASHED:
+        rho = 0.0
+    else:
+        rho = 1.0 / BEHAVIOR_SOLID_PROBABILITY
+    delta = REWARD + DISCOUNT * np.dot(FEATURES[nextState, :], weights) - \
+            np.dot(FEATURES[state, :], weights)
+    delta *= rho * alpha
+    # derivatives happen to be the same matrix due to the linearity
+    weights += FEATURES[state, :] * delta
+    return nextState
 
-# Figure 11.1, Baird's counterexample
-def figure11_1():
+# Semi-gradient DP
+# @weights: weight for each component of the feature vector
+# @alpha: step size
+def semiGradientDP(weights, alpha):
+    delta = 0.0
+    # go through all the states
+    for currentState in STATES:
+        expectedReturn = 0.0
+        # compute bellman error for each state
+        for nextState in STATES:
+            if nextState == LOWER_STATE:
+                expectedReturn += REWARD + DISCOUNT * np.dot(weights, FEATURES[nextState, :])
+        bellmanError = expectedReturn - np.dot(weights, FEATURES[currentState, :])
+        # accumulate gradients
+        delta += bellmanError * FEATURES[currentState, :]
+    # derivatives happen to be the same matrix due to the linearity
+    weights += alpha / len(STATES) * delta
+
+figureIndex = 0
+
+# Figure 11.2(a), Baird's counterexample
+def figure11_2_a():
     # Initial the weights
     weights = np.ones(FEATURE_SIZE)
     weights[6] = 10
 
-    # pick up a small alpha
-    alpha = 1e-3
+    alpha = 0.01
 
-    episodes = 100
-    thetas = np.zeros((FEATURE_SIZE, episodes))
-    for episode in range(episodes):
-        semiGradientOffPolicyTD(weights, alpha)
-        thetas[:, episode] = weights
+    steps = 1000
+    thetas = np.zeros((FEATURE_SIZE, steps))
+    state = np.random.choice(STATES)
+    for step in range(steps):
+        state = semiGradientOffPolicyTD(state, weights, alpha)
+        thetas[:, step] = weights
 
-    plt.figure(0)
+    global figureIndex
+    plt.figure(figureIndex)
+    figureIndex += 1
     for i in range(FEATURE_SIZE):
         plt.plot(thetas[i, :], label='theta' + str(i + 1))
-    plt.xlabel('Episodes')
+    plt.xlabel('Steps')
     plt.ylabel('Theta value')
     plt.legend()
 
-figure11_1()
-plt.show()
+def figure11_2_b():
+    # Initial the weights
+    weights = np.ones(FEATURE_SIZE)
+    weights[6] = 10
+
+    alpha = 0.01
+
+    sweeps = 1000
+    thetas = np.zeros((FEATURE_SIZE, sweeps))
+    for sweep in range(sweeps):
+        semiGradientDP(weights, alpha)
+        thetas[:, sweep] = weights
+
+    global figureIndex
+    plt.figure(figureIndex)
+    figureIndex += 1
+    for i in range(FEATURE_SIZE):
+        plt.plot(thetas[i, :], label='theta' + str(i + 1))
+    plt.xlabel('Sweeps')
+    plt.ylabel('Theta value')
+    plt.legend()
+
+if __name__ == '__main__':
+    figure11_2_a()
+    figure11_2_b()
+    plt.show()
