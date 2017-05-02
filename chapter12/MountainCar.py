4762f7f10a24da2858f162f2ae18a6ae4ff3962e
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-05-02 12:48:09 -0600

    Figure 12.10

--- chapter12/MountainCar.py
+++ chapter12/MountainCar.py
@@ -32,6 +32,9 @@ DISCOUNT = 1.0
 # use optimistic initial value, so it's ok to set epsilon to 0
 EPSILON = 0
 
+# maximum steps per episode
+STEP_LIMIT = 5000
+
 # take an @action at @position and @velocity
 # @return: new position, new velocity, reward (always -1)
 def takeAction(position, velocity, action):
@@ -44,13 +47,14 @@ def takeAction(position, velocity, action):
         newVelocity = 0.0
     return newPosition, newVelocity, reward
 
+# I use underline name convention for the following for trace functions to make use of '__name__'
+
 # accumulating trace update rule
 # @trace: old trace (will be modified)
 # @activeTiles: current active tile indices
 # @lam: lambda
-# @alpha: step size for all tiles
 # @return: new trace for convenience
-def accumulatingTrace(trace, activeTiles, lam, alpha):
+def accumulating_trace(trace, activeTiles, lam):
     trace *= lam * DISCOUNT
     trace[activeTiles] += 1
     return trace
@@ -59,12 +63,24 @@ def accumulatingTrace(trace, activeTiles, lam, alpha):
 # @trace: old trace (will be modified)
 # @activeTiles: current active tile indices
 # @lam: lambda
-# @alpha: step size for all tiles
 # @return: new trace for convenience
-def replacingTrace(trace, activeTiles, lam, alpha):
+def replacing_trace(trace, activeTiles, lam):
     active = np.in1d(np.arange(len(trace)), activeTiles)
     trace[active] = 1
     trace[~active] *= lam * DISCOUNT
+    return trace
+
+# replacing trace update rule, 'clearing' means set all tiles corresponding to non-selected actions to 0
+# @trace: old trace (will be modified)
+# @activeTiles: current active tile indices
+# @lam: lambda
+# @clearingTiles: tiles to be cleared
+# @return: new trace for convenience
+def replacing_trace_with_clearing(trace, activeTiles, lam, clearingTiles):
+    active = np.in1d(np.arange(len(trace)), activeTiles)
+    trace[~active] *= lam * DISCOUNT
+    trace[clearingTiles] = 0
+    trace[active] = 1
     return trace
 
 # dutch trace update rule
@@ -73,7 +89,7 @@ def replacingTrace(trace, activeTiles, lam, alpha):
 # @lam: lambda
 # @alpha: step size for all tiles
 # @return: new trace for convenience
-def dutchTrace(trace, activeTiles, lam, alpha):
+def dutch_trace(trace, activeTiles, lam, alpha):
     coef = 1 - alpha * DISCOUNT * lam * np.sum(trace[activeTiles])
     trace *= DISCOUNT * lam
     trace[activeTiles] += coef
@@ -87,7 +103,7 @@ class Sarsa:
     # View the following webpage for more information
     # http://incompleteideas.net/sutton/tiles/tiles3.html
     # @maxSize: the maximum # of indices
-    def __init__(self, stepSize, lam, traceUpdate=accumulatingTrace, numOfTilings=8, maxSize=2048):
+    def __init__(self, stepSize, lam, traceUpdate=accumulating_trace, numOfTilings=8, maxSize=2048):
         self.maxSize = maxSize
         self.numOfTilings = numOfTilings
         self.traceUpdate = traceUpdate
@@ -129,7 +145,18 @@ class Sarsa:
         activeTiles = self.getActiveTiles(position, velocity, action)
         estimation = np.sum(self.weights[activeTiles])
         delta = target - estimation
-        self.traceUpdate(self.trace, activeTiles, self.lam, self.stepSize)
+        if self.traceUpdate == accumulating_trace or self.traceUpdate == replacing_trace:
+            self.traceUpdate(self.trace, activeTiles, self.lam)
+        elif self.traceUpdate == dutch_trace:
+            self.traceUpdate(self.trace, activeTiles, self.lam, self.stepSize)
+        elif self.traceUpdate == replacing_trace_with_clearing:
+            clearingTiles = []
+            for act in ACTIONS:
+                if act != action:
+                    clearingTiles.extend(self.getActiveTiles(position, velocity, act))
+            self.traceUpdate(self.trace, activeTiles, self.lam, clearingTiles)
+        else:
+            raise Exception('Unexpected Trace Type')
         self.weights += self.stepSize * delta * self.trace
 
     # get # of steps to reach the goal under current state value function
@@ -166,33 +193,36 @@ def play(evaluator):
         action = nextAction
         if nextPosition == POSITION_MAX:
             break
-        if steps > 5000:
-            print('Step limit exceeded!')
+        if steps >= STEP_LIMIT:
+            print('Step Limit Exceeded!')
             break
     return steps
 
 figureIndex = 0
 
 # figure 12.9, effect of the lambda and alpha on early performance of Sarsa(lambda)
-def figure12_9():
+def figure12_9(load=False):
     runs = 30
     episodes = 50
     alphas = np.arange(1, 8) / 4.0
     lams = [0.99, 0.95, 0.5, 0]
-    steps = np.zeros((len(lams), len(alphas), runs, episodes))
-    for lamInd, lam in enumerate(lams):
-        for alphaInd, alpha in enumerate(alphas):
-            for run in range(runs):
-                evaluator = Sarsa(alpha, lam, replacingTrace)
-                for ep in range(episodes):
-                    step = play(evaluator)
-                    steps[lamInd, alphaInd, run, ep] = step
-                    print('lambda %f, alpha %f, run %d, episode %d, steps %d' %
-                          (lam, alpha, run, ep, step))
-    with open('figure12_9.bin', 'wb') as f:
-        pickle.dump(steps, f)
-    # with open('figure12_9.bin', 'rb') as f:
-    #     steps = pickle.load(f)
+
+    if load:
+        with open('figure12_9.bin', 'rb') as f:
+            steps = pickle.load(f)
+    else:
+        steps = np.zeros((len(lams), len(alphas), runs, episodes))
+        for lamInd, lam in enumerate(lams):
+            for alphaInd, alpha in enumerate(alphas):
+                for run in range(runs):
+                    evaluator = Sarsa(alpha, lam, replacing_trace)
+                    for ep in range(episodes):
+                        step = play(evaluator)
+                        steps[lamInd, alphaInd, run, ep] = step
+                        print('lambda %f, alpha %f, run %d, episode %d, steps %d' %
+                              (lam, alpha, run, ep, step))
+        with open('figure12_9.bin', 'wb') as f:
+            pickle.dump(steps, f)
 
     # average over episodes
     steps = np.mean(steps, axis=3)
@@ -210,8 +240,54 @@ def figure12_9():
     plt.ylim([180, 300])
     plt.legend()
 
+# figure 12.10, summary comparision of Sarsa(lambda) algorithms
+# I use 8 tilings rather than 10 tilings
+def figure12_10(load=False):
+    traceTypes = [dutch_trace, replacing_trace, replacing_trace_with_clearing, accumulating_trace]
+    alphas = np.arange(0.2, 2.2, 0.2)
+    episodes = 20
+    runs = 30
+    lam = 0.9
+    rewards = np.zeros((len(traceTypes), len(alphas), runs, episodes))
+
+    if load:
+        with open('figure12_10.bin', 'rb') as f:
+            rewards = pickle.load(f)
+    else:
+        for traceInd, trace in enumerate(traceTypes):
+            for alphaInd, alpha in enumerate(alphas):
+                for run in range(runs):
+                    evaluator = Sarsa(alpha, lam, trace)
+                    for ep in range(episodes):
+                        if trace == accumulating_trace and alpha > 0.6:
+                            steps = STEP_LIMIT
+                        else:
+                            steps = play(evaluator)
+                        rewards[traceInd, alphaInd, run, ep] = -steps
+                        print('%s, step size %f, run %d, episode %d, rewards %d' %
+                              (trace.__name__, alpha, run, ep, -steps))
+        with open('figure12_10.bin', 'wb') as f:
+            pickle.dump(rewards, f)
+
+    # average over episodes
+    rewards = np.mean(rewards, axis=3)
+
+    # average over runs
+    rewards = np.mean(rewards, axis=2)
+
+    global figureIndex
+    plt.figure(figureIndex)
+    figureIndex += 1
+    for traceInd, trace in enumerate(traceTypes):
+        plt.plot(alphas, rewards[traceInd, :], label=trace.__name__)
+    plt.xlabel('alpha * # of tilings (8)')
+    plt.ylabel('averaged rewards pre episode')
+    plt.ylim([-550, -150])
+    plt.legend()
+
 if __name__ == '__main__':
-    figure12_9()
+    # figure12_9()
+    figure12_10()
     plt.show()
 
 
