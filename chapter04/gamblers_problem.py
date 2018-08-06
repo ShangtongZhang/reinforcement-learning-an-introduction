8a5e428cacb6879b4410b653b9871459316c071f
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-06 11:15:11 -0600

    Refactor gamblers problem

---
+++ chapter04/gamblers_problem.py
@@ -0,0 +1,66 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+# goal
+GOAL = 100
+
+# all states, including state 0 and state 100
+STATES = np.arange(GOAL + 1)
+
+# probability of head
+HEAD_PROB = 0.4
+
+def figure_4_3():
+    # state value
+    state_value = np.zeros(GOAL + 1)
+    state_value[GOAL] = 1.0
+
+    # value iteration
+    while True:
+        delta = 0.0
+        for state in STATES[1:GOAL]:
+            # get possilbe actions for current state
+            actions = np.arange(min(state, GOAL - state) + 1)
+            action_returns = []
+            for action in actions:
+                action_returns.append(
+                    HEAD_PROB * state_value[state + action] + (1 - HEAD_PROB) * state_value[state - action])
+            new_value = np.max(action_returns)
+            delta += np.abs(state_value[state] - new_value)
+            # update state value
+            state_value[state] = new_value
+        if delta < 1e-9:
+            break
+
+    # compute the optimal policy
+    policy = np.zeros(GOAL + 1)
+    for state in STATES[1:GOAL]:
+        actions = np.arange(min(state, GOAL - state) + 1)
+        action_returns = []
+        for action in actions:
+            action_returns.append(
+                HEAD_PROB * state_value[state + action] + (1 - HEAD_PROB) * state_value[state - action])
+        # due to tie, can't reproduce the optimal policy in book
+        policy[state] = actions[np.argmax(action_returns)]
+
+    plt.subplot(2, 1, 1)
+    plt.plot(state_value)
+    plt.ylabel('Value estimates')
+
+    plt.subplot(2, 1, 2)
+    plt.scatter(STATES, policy)
+    plt.xlabel('Capital')
+    plt.ylabel('Final policy (stake)')
+
+    plt.savefig('../images/figure_4_3.png')
+    plt.close()
+
+if __name__ == '__main__':
+    figure_4_3()
