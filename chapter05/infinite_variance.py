7c26bbd578ded727ad2981d12375e212a2589415
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-06 15:27:46 -0600

    Refactor figure 5.4

--- chapter05/infinite_variance.py
+++ chapter05/infinite_variance.py
@@ -1,24 +1,25 @@
 #######################################################################
 # Copyright (C)                                                       #
-# 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
 # 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
 
-from __future__ import print_function
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 ACTION_BACK = 0
 ACTION_END = 1
 
 # behavior policy
-def behaviorPolicy():
+def behavior_policy():
     return np.random.binomial(1, 0.5)
 
 # target policy
-def targetPolicy():
+def target_policy():
     return ACTION_BACK
 
 # one turn
@@ -26,33 +27,34 @@ def play():
     # track the action for importance ratio
     trajectory = []
     while True:
-        action = behaviorPolicy()
+        action = behavior_policy()
         trajectory.append(action)
         if action == ACTION_END:
             return 0, trajectory
         if np.random.binomial(1, 0.9) == 0:
             return 1, trajectory
 
-# Figure 5.5
-def monteCarloSample():
+def figure_5_4():
     runs = 10
     episodes = 100000
-    axisX = np.log10(np.arange(1, episodes + 1))
-    for run in range(0, runs):
-        sumOfRewards = [0]
+    for run in range(runs):
+        rewards = []
         for episode in range(0, episodes):
             reward, trajectory = play()
             if trajectory[-1] == ACTION_END:
-                importanceRatio = 0
+                rho = 0
             else:
-                importanceRatio = 1.0 / pow(0.5, len(trajectory))
-            sumOfRewards.append(sumOfRewards[-1] + importanceRatio * reward)
-        del sumOfRewards[0]
-        estimations = np.asarray(sumOfRewards) / np.arange(1, episodes + 1)
-        plt.plot(axisX, estimations)
-    plt.xlabel('Episodes (10^x)')
+                rho = 1.0 / pow(0.5, len(trajectory))
+            rewards.append(rho * reward)
+        rewards = np.add.accumulate(rewards)
+        estimations = np.asarray(rewards) / np.arange(1, episodes + 1)
+        plt.plot(estimations)
+    plt.xlabel('Episodes (log scale)')
     plt.ylabel('Ordinary Importance Sampling')
-    plt.show()
-    return
+    plt.xscale('log')
 
-monteCarloSample()
+    plt.savefig('../images/figure_5_4.png')
+    plt.close()
+
+if __name__ == '__main__':
+    figure_5_4()
