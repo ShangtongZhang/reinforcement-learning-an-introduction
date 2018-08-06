7ff33b4f3bcaae9b474210577669f18003978815
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-06 12:09:14 -0600

    Refactor figure 5.1

--- chapter05/Blackjack.py
+++ chapter05/Blackjack.py
@@ -1,207 +1,208 @@
 #######################################################################
 # Copyright (C)                                                       #
-# 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
 # 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # 2017 Nicky van Foreest(vanforeest@gmail.com)                        #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
 
-from __future__ import print_function
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
 from mpl_toolkits.mplot3d import Axes3D
 
 # actions: hit or stand
 ACTION_HIT = 0
 ACTION_STAND = 1  #  "strike" in the book
-actions = [ACTION_HIT, ACTION_STAND]
+ACTIONS = [ACTION_HIT, ACTION_STAND]
 
 # policy for player
-policyPlayer = np.zeros(22)
+POLICY_PLAYER = np.zeros(22)
 for i in range(12, 20):
-    policyPlayer[i] = ACTION_HIT
-policyPlayer[20] = ACTION_STAND
-policyPlayer[21] = ACTION_STAND
+    POLICY_PLAYER[i] = ACTION_HIT
+POLICY_PLAYER[20] = ACTION_STAND
+POLICY_PLAYER[21] = ACTION_STAND
 
 # function form of target policy of player
-def targetPolicyPlayer(usableAcePlayer, playerSum, dealerCard):
-    return policyPlayer[playerSum]
+def target_policy_player(usable_ace_player, player_sum, dealer_card):
+    return POLICY_PLAYER[player_sum]
 
 # function form of behavior policy of player
-def behaviorPolicyPlayer(usableAcePlayer, playerSum, dealerCard):
+def behavior_policy_player(usable_ace_player, player_sum, dealer_card):
     if np.random.binomial(1, 0.5) == 1:
         return ACTION_STAND
     return ACTION_HIT
 
 # policy for dealer
-policyDealer = np.zeros(22)
+POLICY_DEALER = np.zeros(22)
 for i in range(12, 17):
-    policyDealer[i] = ACTION_HIT
+    POLICY_DEALER[i] = ACTION_HIT
 for i in range(17, 22):
-    policyDealer[i] = ACTION_STAND
+    POLICY_DEALER[i] = ACTION_STAND
 
 # get a new card
-def getCard():
+def get_card():
     card = np.random.randint(1, 14)
     card = min(card, 10)
     return card
 
 # play a game
-# @policyPlayerFn: specify policy for player
-# @initialState: [whether player has a usable Ace, sum of player's cards, one card of dealer]
-# @initialAction: the initial action
-def play(policyPlayerFn, initialState=None, initialAction=None):
+# @policy_player: specify policy for player
+# @initial_state: [whether player has a usable Ace, sum of player's cards, one card of dealer]
+# @initial_action: the initial action
+def play(policy_player, initial_state=None, initial_action=None):
     # player status
 
     # sum of player
-    playerSum = 0
+    player_sum = 0
 
     # trajectory of player
-    playerTrajectory = []
+    player_trajectory = []
 
     # whether player uses Ace as 11
-    usableAcePlayer = False
+    usable_ace_player = False
 
     # dealer status
-    dealerCard1 = 0
-    dealerCard2 = 0
-    usableAceDealer = False
+    dealer_card1 = 0
+    dealer_card2 = 0
+    usable_ace_dealer = False
 
-    if initialState is None:
+    if initial_state is None:
         # generate a random initial state
 
-        numOfAce = 0
+        num_of_ace = 0
 
         # initialize cards of player
-        while playerSum < 12:
+        while player_sum < 12:
             # if sum of player is less than 12, always hit
-            card = getCard()
+            card = get_card()
 
             # if get an Ace, use it as 11
             if card == 1:
-                numOfAce += 1
+                num_of_ace += 1
                 card = 11
-                usableAcePlayer = True
-            playerSum += card
+                usable_ace_player = True
+            player_sum += card
 
         # if player's sum is larger than 21, he must hold at least one Ace, two Aces are possible
-        if playerSum > 21:
+        if player_sum > 21:
             # use the Ace as 1 rather than 11
-            playerSum -= 10
+            player_sum -= 10
 
             # if the player only has one Ace, then he doesn't have usable Ace any more
-            if numOfAce == 1:
-                usableAcePlayer = False
+            if num_of_ace == 1:
+                usable_ace_player = False
 
         # initialize cards of dealer, suppose dealer will show the first card he gets
-        dealerCard1 = getCard()
-        dealerCard2 = getCard()
+        dealer_card1 = get_card()
+        dealer_card2 = get_card()
 
     else:
         # use specified initial state
-        usableAcePlayer = initialState[0]
-        playerSum = initialState[1]
-        dealerCard1 = initialState[2]
-        dealerCard2 = getCard()
+        usable_ace_player, player_sum, dealer_card1 = initial_state
+        dealer_card2 = get_card()
 
     # initial state of the game
-    state = [usableAcePlayer, playerSum, dealerCard1]
+    state = [usable_ace_player, player_sum, dealer_card1]
 
     # initialize dealer's sum
-    dealerSum = 0
-    if dealerCard1 == 1 and dealerCard2 != 1:
-        dealerSum += 11 + dealerCard2
-        usableAceDealer = True
-    elif dealerCard1 != 1 and dealerCard2 == 1:
-        dealerSum += dealerCard1 + 11
-        usableAceDealer = True
-    elif dealerCard1 == 1 and dealerCard2 == 1:
-        dealerSum += 1 + 11
-        usableAceDealer = True
+    dealer_sum = 0
+    if dealer_card1 == 1 and dealer_card2 != 1:
+        dealer_sum += 11 + dealer_card2
+        usable_ace_dealer = True
+    elif dealer_card1 != 1 and dealer_card2 == 1:
+        dealer_sum += dealer_card1 + 11
+        usable_ace_dealer = True
+    elif dealer_card1 == 1 and dealer_card2 == 1:
+        dealer_sum += 1 + 11
+        usable_ace_dealer = True
     else:
-        dealerSum += dealerCard1 + dealerCard2
+        dealer_sum += dealer_card1 + dealer_card2
 
     # game starts!
 
     # player's turn
     while True:
-        if initialAction is not None:
-            action = initialAction
-            initialAction = None
+        if initial_action is not None:
+            action = initial_action
+            initial_action = None
         else:
             # get action based on current sum
-            action = policyPlayerFn(usableAcePlayer, playerSum, dealerCard1)
+            action = policy_player(usable_ace_player, player_sum, dealer_card1)
 
         # track player's trajectory for importance sampling
-        playerTrajectory.append([(usableAcePlayer, playerSum, dealerCard1), action])
+        player_trajectory.append([(usable_ace_player, player_sum, dealer_card1), action])
 
         if action == ACTION_STAND:
             break
         # if hit, get new card
-        playerSum += getCard()
+        player_sum += get_card()
 
         # player busts
-        if playerSum > 21:
+        if player_sum > 21:
             # if player has a usable Ace, use it as 1 to avoid busting and continue
-            if usableAcePlayer == True:
-                playerSum -= 10
-                usableAcePlayer = False
+            if usable_ace_player == True:
+                player_sum -= 10
+                usable_ace_player = False
             else:
                 # otherwise player loses
-                return state, -1, playerTrajectory
+                return state, -1, player_trajectory
 
     # dealer's turn
     while True:
         # get action based on current sum
-        action = policyDealer[dealerSum]
+        action = POLICY_DEALER[dealer_sum]
         if action == ACTION_STAND:
             break
         # if hit, get a new card
-        new_card = getCard()
-        if new_card == 1 and dealerSum + 11 < 21:
-            dealerSum += 11
-            usableAceDealer = True
+        new_card = get_card()
+        if new_card == 1 and dealer_sum + 11 < 21:
+            dealer_sum += 11
+            usable_ace_dealer = True
         else:
-            dealerSum += new_card
+            dealer_sum += new_card
         # dealer busts
-        if dealerSum > 21:
-            if usableAceDealer == True:
+        if dealer_sum > 21:
+            if usable_ace_dealer == True:
             # if dealer has a usable Ace, use it as 1 to avoid busting and continue
-                dealerSum -= 10
-                usableAceDealer = False
+                dealer_sum -= 10
+                usable_ace_dealer = False
             else:
             # otherwise dealer loses
-                return state, 1, playerTrajectory
+                return state, 1, player_trajectory
 
     # compare the sum between player and dealer
-    if playerSum > dealerSum:
-        return state, 1, playerTrajectory
-    elif playerSum == dealerSum:
-        return state, 0, playerTrajectory
+    if player_sum > dealer_sum:
+        return state, 1, player_trajectory
+    elif player_sum == dealer_sum:
+        return state, 0, player_trajectory
     else:
-        return state, -1, playerTrajectory
+        return state, -1, player_trajectory
 
 # Monte Carlo Sample with On-Policy
-def monteCarloOnPolicy(nEpisodes):
-    statesUsableAce = np.zeros((10, 10))
+def monte_carlo_on_policy(episodes):
+    states_usable_ace = np.zeros((10, 10))
     # initialze counts to 1 to avoid 0 being divided
-    statesUsableAceCount = np.ones((10, 10))
-    statesNoUsableAce = np.zeros((10, 10))
+    states_usable_ace_count = np.ones((10, 10))
+    states_no_usable_ace = np.zeros((10, 10))
     # initialze counts to 1 to avoid 0 being divided
-    statesNoUsableAceCount = np.ones((10, 10))
-    for i in range(0, nEpisodes):
-        state, reward, playerTrajectory = play(targetPolicyPlayer)
-        for (usableAce, playerSum, dealerCard), _ in playerTrajectory:
-            playerSum -= 12
-            dealerCard -= 1
-            if usableAce:
-                statesUsableAceCount[playerSum, dealerCard] += 1
-                statesUsableAce[playerSum, dealerCard] += reward
+    states_no_usable_ace_count = np.ones((10, 10))
+    for i in tqdm(range(0, episodes)):
+        _, reward, player_trajectory = play(target_policy_player)
+        for (usable_ace, player_sum, dealer_card), _ in player_trajectory:
+            player_sum -= 12
+            dealer_card -= 1
+            if usable_ace:
+                states_usable_ace_count[player_sum, dealer_card] += 1
+                states_usable_ace[player_sum, dealer_card] += reward
             else:
-                statesNoUsableAceCount[playerSum, dealerCard] += 1
-                statesNoUsableAce[playerSum, dealerCard] += reward
-    return statesUsableAce / statesUsableAceCount, statesNoUsableAce / statesNoUsableAceCount
+                states_no_usable_ace_count[player_sum, dealer_card] += 1
+                states_no_usable_ace[player_sum, dealer_card] += reward
+    return states_usable_ace / states_usable_ace_count, states_no_usable_ace / states_no_usable_ace_count
 
 # Monte Carlo with Exploring Starts
 def monteCarloES(nEpisodes):
@@ -227,7 +228,7 @@ def monteCarloES(nEpisodes):
         initialState = [bool(np.random.choice([0, 1])),
                        np.random.choice(range(12, 22)),
                        np.random.choice(range(1, 11))]
-        initialAction = np.random.choice(actions)
+        initialAction = np.random.choice(ACTIONS)
         _, reward, trajectory = play(behaviorPolicy, initialState, initialAction)
         for (usableAce, playerSum, dealerCard), action in trajectory:
             usableAce = int(usableAce)
@@ -245,13 +246,13 @@ def monteCarloOffPolicy(nEpisodes):
     sumOfImportanceRatio = [0]
     sumOfRewards = [0]
     for i in range(0, nEpisodes):
-        _, reward, playerTrajectory = play(behaviorPolicyPlayer, initialState=initialState)
+        _, reward, playerTrajectory = play(behavior_policy_player, initial_state=initialState)
 
         # get the importance ratio
         importanceRatioAbove = 1.0
         importanceRatioBelow = 1.0
         for (usableAce, playerSum, dealerCard), action in playerTrajectory:
-            if action == targetPolicyPlayer(usableAce, playerSum, dealerCard):
+            if action == target_policy_player(usableAce, playerSum, dealerCard):
                 importanceRatioBelow *= 0.5
             else:
                 importanceRatioAbove = 0.0
@@ -292,15 +293,29 @@ def prettyPrint(data, tile, zlabel='reward'):
     ax.set_ylabel('dealer showing')
     ax.set_zlabel(zlabel)
 
-# Figure 5.1
-def onPolicy():
-    statesUsableAce1, statesNoUsableAce1 = monteCarloOnPolicy(10000)
-    statesUsableAce2, statesNoUsableAce2 = monteCarloOnPolicy(500000)
-    prettyPrint(statesUsableAce1, 'Usable Ace, 10000 Episodes')
-    prettyPrint(statesNoUsableAce1, 'No Usable Ace, 10000 Episodes')
-    prettyPrint(statesUsableAce2, 'Usable Ace, 500000 Episodes')
-    prettyPrint(statesNoUsableAce2, 'No Usable Ace, 500000 Episodes')
-    plt.show()
+def figure_5_1():
+    states_usable_ace_1, states_no_usable_ace_1 = monte_carlo_on_policy(10000)
+    states_usable_ace_2, states_no_usable_ace_2 = monte_carlo_on_policy(500000)
+
+    states = [states_usable_ace_1, states_no_usable_ace_1,
+              states_usable_ace_2, states_no_usable_ace_2]
+
+    titles = ['Usable Ace, 10000 Episodes', 'No Usable Ace, 10000 Episodes',
+              'Usable Ace, 500000 Episodes', 'No Usable Ace, 500000 Episodes']
+
+    _, axes = plt.subplots(2, 2, figsize=(40, 30))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+
+    for state, title, axis in zip(states, titles, axes):
+        fig = sns.heatmap(state, cmap="YlGnBu", ax=axis, xticklabels=range(1, 11),
+                          yticklabels=range(12, 22))
+        fig.set_ylabel('player sum')
+        fig.set_xlabel('dealer showing')
+        fig.set_title(title)
+
+    plt.savefig('../images/figure_5_1.png')
+    plt.close()
 
 # Figure 5.3
 def figure5_3():
@@ -345,8 +360,6 @@ def offPolicy():
     plt.show()
 
 
-if __name__ == "__main__":
-    onPolicy()
-    figure5_3()
-    offPolicy()
+if __name__ == '__main__':
+    figure_5_1()
 
