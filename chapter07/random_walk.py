a4fa0d10df1814f39990a20c5b0742c87240eab4
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-06 18:18:26 -0600

    Refactor figure 7.2

--- chapter07/random_walk.py
+++ chapter07/random_walk.py
@@ -1,14 +1,16 @@
 #######################################################################
 # Copyright (C)                                                       #
-# 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
 # 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
 
-from __future__ import print_function
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # all states
 N_STATES = 19
@@ -16,11 +18,8 @@ N_STATES = 19
 # discount
 GAMMA = 1
 
-# initial state values
-stateValues = np.zeros(N_STATES + 2)
-
 # all states but terminal states
-states = np.arange(1, N_STATES + 1)
+STATES = np.arange(1, N_STATES + 1)
 
 # start from the middle state
 START_STATE = 10
@@ -31,20 +30,20 @@ START_STATE = 10
 END_STATES = [0, N_STATES + 1]
 
 # true state value from bellman equation
-realStateValues = np.arange(-20, 22, 2) / 20.0
-realStateValues[0] = realStateValues[-1] = 0
+TRUE_VALUE = np.arange(-20, 22, 2) / 20.0
+TRUE_VALUE[0] = TRUE_VALUE[-1] = 0
 
 # n-steps TD method
-# @stateValues: values for each state, will be updated
+# @value: values for each state, will be updated
 # @n: # of steps
 # @alpha: # step size
-def temporalDifference(stateValues, n, alpha):
+def temporal_difference(value, n, alpha):
     # initial starting state
-    currentState = START_STATE
+    state = START_STATE
 
     # arrays to store states and rewards for an episode
     # space isn't a major consideration, so I didn't use the mod trick
-    states = [currentState]
+    states = [state]
     rewards = [0]
 
     # track the time
@@ -59,46 +58,44 @@ def temporalDifference(stateValues, n, alpha):
         if time < T:
             # choose an action randomly
             if np.random.binomial(1, 0.5) == 1:
-                newState = currentState + 1
+                next_state = state + 1
             else:
-                newState = currentState - 1
-            if newState == 0:
+                next_state = state - 1
+
+            if next_state == 0:
                 reward = -1
-            elif newState == 20:
+            elif next_state == 20:
                 reward = 1
             else:
                 reward = 0
 
             # store new state and new reward
-            states.append(newState)
+            states.append(next_state)
             rewards.append(reward)
 
-            if newState in END_STATES:
+            if next_state in END_STATES:
                 T = time
 
         # get the time of the state to update
-        updateTime = time - n
-        if updateTime >= 0:
+        update_time = time - n
+        if update_time >= 0:
             returns = 0.0
             # calculate corresponding rewards
-            for t in range(updateTime + 1, min(T, updateTime + n) + 1):
-                returns += pow(GAMMA, t - updateTime - 1) * rewards[t]
+            for t in range(update_time + 1, min(T, update_time + n) + 1):
+                returns += pow(GAMMA, t - update_time - 1) * rewards[t]
             # add state value to the return
-            if updateTime + n <= T:
-                returns += pow(GAMMA, n) * stateValues[states[(updateTime + n)]]
-            stateToUpdate = states[updateTime]
+            if update_time + n <= T:
+                returns += pow(GAMMA, n) * value[states[(update_time + n)]]
+            state_to_update = states[update_time]
             # update the state value
-            if not stateToUpdate in END_STATES:
-                stateValues[stateToUpdate] += alpha * (returns - stateValues[stateToUpdate])
-        if updateTime == T - 1:
+            if not state_to_update in END_STATES:
+                value[state_to_update] += alpha * (returns - value[state_to_update])
+        if update_time == T - 1:
             break
-        currentState = newState
+        state = next_state
 
 # Figure 7.2, it will take quite a while
 def figure7_2():
-    # truncate value for better display
-    truncateValue = 0.55
-
     # all possible steps
     steps = np.power(2, np.arange(0, 10))
 
@@ -113,25 +110,29 @@ def figure7_2():
 
     # track the errors for each (step, alpha) combination
     errors = np.zeros((len(steps), len(alphas)))
-    for run in range(0, runs):
-        for stepInd, step in zip(range(len(steps)), steps):
-            for alphaInd, alpha in zip(range(len(alphas)), alphas):
-                print('run:', run, 'step:', step, 'alpha:', alpha)
-                currentStateValues = np.copy(stateValues)
+    for run in tqdm(range(0, runs)):
+        for step_ind, step in zip(range(len(steps)), steps):
+            for alpha_ind, alpha in zip(range(len(alphas)), alphas):
+                # print('run:', run, 'step:', step, 'alpha:', alpha)
+                value = np.zeros(N_STATES + 2)
                 for ep in range(0, episodes):
-                    temporalDifference(currentStateValues, step, alpha)
+                    temporal_difference(value, step, alpha)
                     # calculate the RMS error
-                    errors[stepInd, alphaInd] += np.sqrt(np.sum(np.power(currentStateValues - realStateValues, 2)) / N_STATES)
+                    errors[step_ind, alpha_ind] += np.sqrt(np.sum(np.power(value - TRUE_VALUE, 2)) / N_STATES)
     # take average
     errors /= episodes * runs
-    # truncate the error
-    errors[errors > truncateValue] = truncateValue
-    plt.figure()
+
     for i in range(0, len(steps)):
-        plt.plot(alphas, errors[i, :], label='n = ' + str(steps[i]))
+        plt.plot(alphas, errors[i, :], label='n = %d' % (steps[i]))
     plt.xlabel('alpha')
     plt.ylabel('RMS error')
+    plt.ylim([0.25, 0.55])
     plt.legend()
 
-figure7_2()
-plt.show()
+    plt.savefig('../images/figure_7_2.png')
+    plt.close()
+
+if __name__ == '__main__':
+    figure7_2()
+
+
