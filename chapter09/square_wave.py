48c6bedf52dfef0cffaa24f480da0ece2b5ba792
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-08 20:54:41 -0600

    Refactor figure 9.8

--- chapter09/square_wave.py
+++ chapter09/square_wave.py
@@ -1,14 +1,16 @@
 #######################################################################
 # Copyright (C)                                                       #
-# 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
 # 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
 
-from __future__ import print_function
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # wrapper class for an interval
 # readability is more important than efficiency, so I won't use many tricks
@@ -27,10 +29,10 @@ class Interval:
         return self.right - self.left
 
 # domain of the square wave, [0, 2)
-domain = Interval(0.0, 2.0)
+DOMAIN = Interval(0.0, 2.0)
 
 # square wave function
-def squareWave(x):
+def square_wave(x):
     if 0.5 < x < 1.5:
         return 1
     return 0
@@ -39,8 +41,8 @@ def squareWave(x):
 def sample(n):
     samples = []
     for i in range(0, n):
-        x = np.random.uniform(domain.left, domain.right)
-        y = squareWave(x)
+        x = np.random.uniform(DOMAIN.left, DOMAIN.right)
+        y = square_wave(x)
         samples.append([x, y])
     return samples
 
@@ -48,68 +50,72 @@ def sample(n):
 class ValueFunction:
     # @domain: domain of this function, an instance of Interval
     # @alpha: basic step size for one update
-    def __init__(self, featureWidth, domain=domain, alpha=0.2, numOfFeatures=50):
-        self.featureWidth = featureWidth
-        self.numOfFeatrues = numOfFeatures
+    def __init__(self, feature_width, domain=DOMAIN, alpha=0.2, num_of_features=50):
+        self.feature_width = feature_width
+        self.num_of_featrues = num_of_features
         self.features = []
         self.alpha = alpha
         self.domain = domain
 
         # there are many ways to place those feature windows,
         # following is just one possible way
-        step = (domain.size() - featureWidth) / (numOfFeatures - 1)
+        step = (domain.size() - feature_width) / (num_of_features - 1)
         left = domain.left
-        for i in range(0, numOfFeatures - 1):
-            self.features.append(Interval(left, left + featureWidth))
+        for i in range(0, num_of_features - 1):
+            self.features.append(Interval(left, left + feature_width))
             left += step
         self.features.append(Interval(left, domain.right))
 
         # initialize weight for each feature
-        self.weights = np.zeros(numOfFeatures)
+        self.weights = np.zeros(num_of_features)
 
     # for point @x, return the indices of corresponding feature windows
-    def getActiveFeatures(self, x):
-        activeFeatures = []
+    def get_active_features(self, x):
+        active_features = []
         for i in range(0, len(self.features)):
             if self.features[i].contain(x):
-                activeFeatures.append(i)
-        return activeFeatures
+                active_features.append(i)
+        return active_features
 
     # estimate the value for point @x
     def value(self, x):
-        activeFeatures = self.getActiveFeatures(x)
-        return np.sum(self.weights[activeFeatures])
+        active_features = self.get_active_features(x)
+        return np.sum(self.weights[active_features])
 
     # update weights given sample of point @x
     # @delta: y - x
     def update(self, delta, x):
-        activeFeatures = self.getActiveFeatures(x)
-        delta *= self.alpha / len(activeFeatures)
-        for index in activeFeatures:
+        active_features = self.get_active_features(x)
+        delta *= self.alpha / len(active_features)
+        for index in active_features:
             self.weights[index] += delta
 
-# train @valueFunction with a set of samples @samples
-def approximate(samples, valueFunction):
+# train @value_function with a set of samples @samples
+def approximate(samples, value_function):
     for x, y in samples:
-        delta = y - valueFunction.value(x)
-        valueFunction.update(delta, x)
+        delta = y - value_function.value(x)
+        value_function.update(delta, x)
 
 # Figure 9.8
-def figure9_8():
-    numOfSamples = [10, 40, 160, 2560, 10240]
-    featureWidths = [0.2, 0.4, 1.0]
-    axisX = np.arange(domain.left, domain.right, 0.02)
-    for numOfSample, index in zip(numOfSamples, range(0, len(numOfSamples))):
-        print(numOfSample, 'samples')
-        samples = sample(numOfSample)
-        valueFunctions = [ValueFunction(featureWidth) for featureWidth in featureWidths]
-        plt.figure(index)
-        plt.title(str(numOfSample) + ' samples')
-        for valueFunction in valueFunctions:
-            approximate(samples, valueFunction)
-            values = [valueFunction.value(x) for x in axisX]
-            plt.plot(axisX, values, label='feature width: ' + str(valueFunction.featureWidth))
+def figure_9_8():
+    num_of_samples = [10, 40, 160, 640, 2560, 10240]
+    feature_widths = [0.2, 0.4, 1.0]
+    plt.figure(figsize=(30, 20))
+    axis_x = np.arange(DOMAIN.left, DOMAIN.right, 0.02)
+    for index, num_of_sample in enumerate(num_of_samples):
+        print(num_of_sample, 'samples')
+        samples = sample(num_of_sample)
+        value_functions = [ValueFunction(feature_width) for feature_width in feature_widths]
+        plt.subplot(2, 3, index + 1)
+        plt.title('%d samples' % (num_of_sample))
+        for value_function in value_functions:
+            approximate(samples, value_function)
+            values = [value_function.value(x) for x in axis_x]
+            plt.plot(axis_x, values, label='feature width %.01f' % (value_function.feature_width))
         plt.legend()
 
-figure9_8()
-plt.show()
+    plt.savefig('../images/figure_9_8.png')
+    plt.close()
+
+if __name__ == '__main__':
+    figure_9_8()
