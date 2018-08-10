3bb8eac39b04248a7e8ca09c0d46f28007b210a0
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-08-10 16:59:39 -0600

    Refactor short corridor

--- chapter13/short_corridor.py
+++ chapter13/short_corridor.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def f(p):
     """ True value of the first state
@@ -21,6 +22,142 @@ def f(p):
         of Bellman equations using known dynamics.
     """
     return (2 * p - 4) / (p * (1 - p))
+
+class ShortCorridor:
+    """
+    Short corridor environment, see Example 13.1
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.state = 0
+
+    def step(self, go_right):
+        """
+        Args:
+            go_right (bool): chosen action
+        Returns:
+            tuple of (reward, episode terminated?)
+        """
+        if self.state == 0 or self.state == 2:
+            if go_right:
+                self.state += 1
+            else:
+                self.state = max(0, self.state - 1)
+        else:
+            if go_right:
+                self.state -= 1
+            else:
+                self.state += 1
+
+        if self.state == 3:
+            # terminal state
+            return 0, True
+        else:
+            return -1, False
+
+def softmax(x):
+    t = np.exp(x - np.max(x))
+    return t / np.sum(t)
+
+class Agent:
+    """
+    Agent that follows algorithm
+    'REINFORNCE Monte-Carlo Policy-Gradient Control (episodic)'
+    """
+    def __init__(self, alpha, gamma):
+        # set values such that initial conditions correspond to left-epsilon greedy
+        self.theta = np.array([-1.47, 1.47])
+        self.alpha = alpha
+        self.gamma = gamma
+        # first column - left, second - right
+        self.x = np.array([[0, 1],
+                           [1, 0]])
+        self.rewards = []
+        self.actions = []
+
+    def get_pi(self):
+        h = np.dot(self.theta, self.x)
+        t = np.exp(h - np.max(h))
+        pmf = t / np.sum(t)
+        # never become deterministic,
+        # guarantees episode finish
+        imin = np.argmin(pmf)
+        epsilon = 0.05
+
+        if pmf[imin] < epsilon:
+            pmf[:] = 1 - epsilon
+            pmf[imin] = epsilon
+
+        return pmf
+
+    def get_p_right(self):
+        return self.get_pi()[1]
+
+    def choose_action(self, reward):
+        if reward is not None:
+            self.rewards.append(reward)
+
+        pmf = self.get_pi()
+        go_right = np.random.uniform() <= pmf[1]
+        self.actions.append(go_right)
+
+        return go_right
+
+    def episode_end(self, last_reward):
+        self.rewards.append(last_reward)
+
+        # learn theta
+        G = np.zeros(len(self.rewards))
+        G[-1] = self.rewards[-1]
+
+        for i in range(2, len(G) + 1):
+            G[-i] = self.gamma * G[-i + 1] + self.rewards[-i]
+
+        gamma_pow = 1
+
+        for i in range(len(G)):
+            if self.actions[i]:
+                j = 1
+            else:
+                j = 0
+
+            pmf = self.get_pi()
+            grad_lnpi = self.x[:, j] - np.dot(self.x, pmf)
+            update = self.alpha * gamma_pow * G[i] * grad_lnpi
+            self.theta += update
+            gamma_pow *= self.gamma
+
+        self.rewards = []
+        self.actions = []
+
+def trial(num_episodes, alpha, gamma):
+    env = ShortCorridor()
+    agent = Agent(alpha=alpha, gamma=gamma)
+
+    g1 = np.zeros(num_episodes)
+    p_right = np.zeros(num_episodes)
+
+    for episode_idx in range(num_episodes):
+        rewards_sum = 0
+        reward = None
+        env.reset()
+
+        while True:
+            go_right = agent.choose_action(reward)
+            reward, episode_end = env.step(go_right)
+            rewards_sum += reward
+
+            if episode_end:
+                agent.episode_end(reward)
+                break
+
+        g1[episode_idx] = rewards_sum
+        p_right[episode_idx] = agent.get_p_right()
+
+
+    return g1
 
 def example_13_1():
     epsilon = 0.05
@@ -50,5 +187,24 @@ def example_13_1():
     plt.savefig('../images/example_13_1.png')
     plt.close()
 
+def figure_13_1():
+    num_trials = 30
+    num_episodes = 1000
+    alpha = 2e-4
+    gamma = 1
+
+    rewards = np.zeros((num_trials, num_episodes))
+
+    for i in tqdm(range(num_trials)):
+        reward = trial(num_episodes, alpha, gamma)
+        rewards[i, :] = reward
+
+    plt.plot(np.arange(num_episodes) + 1, rewards.mean(axis=0), color="blue")
+    plt.ylabel('total reward on episode')
+    plt.xlabel('episode')
+
+    plt.show()
+
 if __name__ == '__main__':
-    example_13_1()
+    # example_13_1()
+    figure_13_1()
