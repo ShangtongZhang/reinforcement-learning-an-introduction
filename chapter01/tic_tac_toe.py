9f831a3fa140129d4e029f1a9437877b8fbf46fd
ainilaha <281761274@qq.com>
2019-06-08 19:39:45 -0500

    Fixed few minor issues in chapter 1 tic_tac_toe: 1.Added self.symbol  in  Player. self.symbol was used without defined in method set_symbol() of class Player 2. Remove unnecessary parenthesis in return method act of class HumanPlayer. 3. Variable 'sum' is rename is 'sum_values' because 'sum' conflicted name keywords python and numpy. 4. Variables newSate, newHas, and isEnd are renamed as new_state new_hash, and is_end respectively for constancy and python code style.

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -15,6 +15,7 @@ BOARD_ROWS = 3
 BOARD_COLS = 3
 BOARD_SIZE = BOARD_ROWS * BOARD_COLS
 
+
 class State:
     def __init__(self):
         # the board is represented by an n * n array,
@@ -67,8 +68,8 @@ class State:
                 return self.end
 
         # whether it's a tie
-        sum = np.sum(np.abs(self.data))
-        if sum == BOARD_ROWS * BOARD_COLS:
+        sum_values = np.sum(np.abs(self.data))
+        if sum_values == BOARD_ROWS * BOARD_COLS:
             self.winner = 0
             self.end = True
             return self.end
@@ -101,17 +102,19 @@ class State:
             print(out)
         print('-------------')
 
+
 def get_all_states_impl(current_state, current_symbol, all_states):
     for i in range(0, BOARD_ROWS):
         for j in range(0, BOARD_COLS):
             if current_state.data[i][j] == 0:
-                newState = current_state.next_state(i, j, current_symbol)
-                newHash = newState.hash()
-                if newHash not in all_states.keys():
-                    isEnd = newState.is_end()
-                    all_states[newHash] = (newState, isEnd)
-                    if not isEnd:
-                        get_all_states_impl(newState, -current_symbol, all_states)
+                new_state = current_state.next_state(i, j, current_symbol)
+                new_hash = new_state.hash()
+                if new_hash not in all_states.keys():
+                    is_end = new_state.is_end()
+                    all_states[new_hash] = (new_state, is_end)
+                    if not is_end:
+                        get_all_states_impl(new_state, -current_symbol, all_states)
+
 
 def get_all_states():
     current_symbol = 1
@@ -121,8 +124,10 @@ def get_all_states():
     get_all_states_impl(current_state, current_symbol, all_states)
     return all_states
 
+
 # all possible board configurations
 all_states = get_all_states()
+
 
 class Judger:
     # @player1: the player who will move first, its chessman will be 1
@@ -167,6 +172,7 @@ class Judger:
                     current_state.print_state()
                 return current_state.winner
 
+
 # AI player
 class Player:
     # @step_size: the step size to update estimations
@@ -177,6 +183,7 @@ class Player:
         self.epsilon = epsilon
         self.states = []
         self.greedy = []
+        self.symbol = 0
 
     def reset(self):
         self.states = []
@@ -250,6 +257,7 @@ class Player:
         with open('policy_%s.bin' % ('first' if self.symbol == 1 else 'second'), 'rb') as f:
             self.estimations = pickle.load(f)
 
+
 # human interface
 # input a number to put a chessman
 # | q | w | e |
@@ -281,7 +289,8 @@ class HumanPlayer:
         data = self.keys.index(key)
         i = data // int(BOARD_COLS)
         j = data % BOARD_COLS
-        return (i, j, self.symbol)
+        return i, j, self.symbol
+
 
 def train(epochs, print_every_n=500):
     player1 = Player(epsilon=0.01)
@@ -303,6 +312,7 @@ def train(epochs, print_every_n=500):
     player1.save_policy()
     player2.save_policy()
 
+
 def compete(turns):
     player1 = Player(epsilon=0)
     player2 = Player(epsilon=0)
@@ -320,6 +330,7 @@ def compete(turns):
         judger.reset()
     print('%d turns, player 1 win %.02f, player 2 win %.02f' % (turns, player1_win / turns, player2_win / turns))
 
+
 # The game is a zero sum game. If both players are playing with an optimal strategy, every game will end in a tie.
 # So we test whether the AI can guarantee at least a tie if it goes second.
 def play():
@@ -336,8 +347,8 @@ def play():
         else:
             print("It is a tie!")
 
+
 if __name__ == '__main__':
     train(int(1e5))
     compete(int(1e3))
     play()
-
